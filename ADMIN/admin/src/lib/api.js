054f4615733a8007bc700ce45ab508fc2b197945
Carlos Sandoval <jawicho@outlook.com>
2025-10-29 09:28:20 -0700

    Agregando direccion de backend sin puerto

--- ADMIN/admin/src/lib/api.js
+++ ADMIN/admin/src/lib/api.js
@@ -1,4 +1,4 @@
 import axios from 'axios';
-export const api = axios.create({ baseURL: 'https://lionfish-app-wkapu.ondigitalocean.app:8080/api' });
+export const api = axios.create({ baseURL: 'https://lionfish-app-wkapu.ondigitalocean.app/api' });
 
-export const API_ORIGIN = import.meta.env.VITE_API_URL || 'https://lionfish-app-wkapu.ondigitalocean.app:8080';
+export const API_ORIGIN = import.meta.env.VITE_API_URL || 'https://lionfish-app-wkapu.ondigitalocean.app';
